13c89b6a2507d2d798078f209b962a6bf76b6ed4
rose <113295030+ros-e2@users.noreply.github.com>
2024-01-09 06:26:53 +0900

    first comt

--- 221-test/src/api/rssFeedAPI.js
+++
@@ -1,8 +0,0 @@
-//설명: 섹션 프론트에 등록된 기사를 반환합니다.
-// rssFeedAPI.js
-
-// RSS 피드는 XML 형식이므로 일반적인 Axios 설정으로는 처리하기 어렵습니다.
-// 이 예시에서는 간단히 URL을 반환하는 함수를 만들었습니다.
-export const getRssFeedUrl = (section) => {
-  return `https://rss.nytimes.com/services/xml/rss/nyt/${section}.xml`;
-};
