a0761bc1d17a1e3484f2884280a286c9cc8db04f
rose <113295030+ros-e2@users.noreply.github.com>
2024-01-09 05:42:04 +0900

    login

--- 221-test/src/api/archiveAPI.js
+++
@@ -1,8 +0,0 @@
-//설명: 주어진 월에 게시된 NYT 기사 배열을 반환하며, 1851년 이후로 거슬러 올라갈 수 있습니다.
-// archiveAPI.js
-
-import apiClient from './apiClient';
-
-export const getArchive = (year, month) => {
-  return apiClient.get(`/archive/v1/${year}/${month}.json`);
-};
